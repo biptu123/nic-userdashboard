eb1affd6c87a41aa30c64e35b043bcec14c3cd4a
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-06-15 20:08:16 +0530

    Font weight changed

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -117,7 +117,7 @@
                                 class="text-gray-500 hover:text-gray-700 focus:outline-none">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                                     stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="6"
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="5"
                                         d="M12 6v.01M12 12v.01M12 18v.01" />
                                 </svg>
                             </button>
