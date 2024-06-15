4525787a05b7db3cf897f52ff1041ad406175392
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-06-16 00:30:28 +0530

    Font weight changed

--- resources/views/auth/forgot.blade.php
+++ resources/views/auth/forgot.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Login')
+@section('title', 'Forgot Password')
 
 @section('content')
     <div class="h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
